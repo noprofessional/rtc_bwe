e0f3b8d2c3ca1c97c19b0c8c389eb8f157bce98f
liuzheng1 <liuzheng1@huya.com>
2021-11-22 17:09:23 +0800

    [fix] 修复日志格式不正确的问题

--- logging.h
+++ logging.h
@@ -1,10 +1,30 @@
 #pragma once
 
 #include <iostream>
+#include <sstream>
 
 #define RTC_LOG(x) RTC_LOG_##x
 
-#define RTC_LOG_LS_WARNING std::cerr
-#define RTC_LOG_LS_ERROR std::cerr
-#define RTC_LOG_LS_INFO std::cout
-
+class Logger
+{
+public:
+    Logger(int level) : level_(level) {}
+    template<class T>
+    Logger& operator<<(const T& val)
+    {
+        os_ << val;
+        return *this;
+    }
+    ~Logger()
+    {
+        if (level_ > 2)
+            std::cout << os_.str() << std::endl;
+        else
+            std::cerr << os_.str() << std::endl;
+    }
+    char level_;
+    std::ostringstream os_;
+};
+#define RTC_LOG_LS_WARNING Logger(1)
+#define RTC_LOG_LS_ERROR Logger(2)
+#define RTC_LOG_LS_INFO Logger(3)
